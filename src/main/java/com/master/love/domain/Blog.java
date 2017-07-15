02d353f28f02cbbd7dd8d317205376562176f2d2
dearmaster windows7 <694114533@qq.com>
2017-07-15 22:24:16 +0800

    .

--- src/main/java/com/master/love/domain/Blog.java
+++ src/main/java/com/master/love/domain/Blog.java
@@ -1,5 +1,6 @@
 package com.master.love.domain;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -14,11 +15,22 @@ public class Blog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
+    @Column(name = "subject")
+    private String subject;
     @Type(type="text")
-    @Column(name = "blog_content")
+    @Column(name = "content")
     private String content;
-    @Column(name = "blog_publish_time")
+    @Column(name = "publish_time")
+    @JSONField(format = "yyyy-MM-dd hh:mm:ss")
     private Date publishTime;
+    @JSONField(format = "yyyy-MM-dd hh:mm:ss")
+    @Column(name = "latest_update_time")
+    private Date latestUpdateTime;
+    @Column(name = "category")
+    @Transient
+    private BlogCategory blogCategory;
+    @Column(name = "viewed_times")
+    private Integer viewedTimes;
 
     public Integer getId() {
         return id;
@@ -26,6 +38,14 @@ public class Blog {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
     public String getContent() {
@@ -44,12 +64,40 @@ public class Blog {
         this.publishTime = publishTime;
     }
 
+    public Date getLatestUpdateTime() {
+        return latestUpdateTime;
+    }
+
+    public void setLatestUpdateTime(Date latestUpdateTime) {
+        this.latestUpdateTime = latestUpdateTime;
+    }
+
+    public BlogCategory getBlogCategory() {
+        return blogCategory;
+    }
+
+    public void setBlogCategory(BlogCategory blogCategory) {
+        this.blogCategory = blogCategory;
+    }
+
+    public Integer getViewedTimes() {
+        return viewedTimes;
+    }
+
+    public void setViewedTimes(Integer viewedTimes) {
+        this.viewedTimes = viewedTimes;
+    }
+
     @Override
     public String toString() {
         return "Blog{" +
                 "id=" + id +
+                ", subject='" + subject + '\'' +
                 ", content='" + content + '\'' +
                 ", publishTime=" + publishTime +
+                ", latestUpdateTime=" + latestUpdateTime +
+                ", blogCategory=" + blogCategory +
+                ", viewedTimes=" + viewedTimes +
                 '}';
     }
 }
